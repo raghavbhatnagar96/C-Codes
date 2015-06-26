be5a5e92fb0639f3167726bfd6d542d9b28a7f93
Raghav Bhatnagar <raghavbhatnagar96@gmail.com>
2015-06-26 22:45:56 +0530

    Just familiarising myself with the datatypes and what not.

---
+++ DataTypes/minmax.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<limits.h>
+
+int main()
+    {
+    printf("Q1. Signed char minimum value: %d\n", SCHAR_MIN );
+    printf("Q2. Signed char maximum value: %d\n", SCHAR_MAX );
+    printf("Q3. Unsigned char minimum value: %d\n", 0 );
+    printf("Q4. Unsigned char maximum value: %d\n", UCHAR_MAX );
+    printf("Q5. Char minimum value: %d\n", CHAR_MIN );
+    printf("Q6. Char maximum value: %d\n", CHAR_MAX );
+    printf("Q7. Signed short minimum value: %d\n", SHRT_MIN );
+    printf("Q8. Signed short maximum value: %d\n", SHRT_MAX );
+    printf("Q9. Unsigned short minimum value: %d\n", 0 );
+    printf("Q10. Unsigned short maximum value: %d\n", USHRT_MAX );
+    printf("Q11. Signed int minimum value: %d\n", INT_MIN );
+    printf("Q12. Signed int maximum value: %d\n", INT_MAX );
+    printf("Q13. Unsigned int minimum value: %u\n", 0 );
+    printf("Q14. Unsigned int maximum value: %u\n", UINT_MAX );
+    printf("Q15. Signed long minimum value: %ld\n", LONG_MIN );
+    printf("Q16. Signed long maximum value: %ld\n", LONG_MAX );
+    printf("Q17. Unsigned long minimum value: %d\n", 0 );
+    printf("Q18. Unsigned long maximum value: %lu\n", ULONG_MAX );
+    printf("Q19. Signed long long minimum value: %lld\n", LLONG_MIN );
+    printf("Q20. Signed long long maximum value: %lld\n", LLONG_MAX );
+    printf("Q21. Unsigned long long minimum value: %d\n", 0 );
+    printf("Q22. Unsigned long long maximum value: %llu\n", ULLONG_MAX );
+    return 1;
+    }
